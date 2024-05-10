e3a249035c2c426c1faa06a918412632efcfa516
kimsh1691 <kimsh1691@gmail.com>
2024-05-10 16:40:49 +0900

    niceToMeetYou 메서드 작성 완료

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,13 +1,15 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){ // model 객체 받아오기
+        model.addAttribute("username", "수현");
         return "greetings"; //greetings.mustache 파일 반환
     }
 }
